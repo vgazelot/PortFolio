240149ef0910b2e9ae415b20dc8184517178bbf5
vgazelot <v.gazelot@gmail.com>
2014-10-16 23:03:43 +0200

    Corrigé ortho

--- projets.php
+++ projets.php
@@ -16,7 +16,7 @@
 						JEUX RPG EN JAVA
 					</span>
 					<span class="titreChoix" id="titreChoixRPG2" style="position absolute; top:8%;">
-						Projet Scolaire
+						Projet Universitaire
 					</span>
 				</div>
 				<div  class = "choix" id="choixBlueNote">
@@ -26,7 +26,7 @@
 						SITE DE RESTAURANT
 					</span>
 					<span class="titreChoix" id="titreChoixBlueNote2" style="position absolute; top:8%;">
-						Projet Scolaire
+						Projet Universitaire
 					</span>
 				</div>
 				<div  class = "choix" id="choixOthelo">
@@ -35,7 +35,7 @@
 						JEUX OTHELO EN C
 					</span>
 					<span class="titreChoix" id="titreChoixOthelo2" style="position absolute; top:8%;">
-						Projet Scolaire
+						Projet Universitaire
 					</span>
 				</div>
 				<div  class = "choix" id="choixSTI">
@@ -44,16 +44,15 @@
 						Projet STI2D
 					</span>
 					<span class="titreChoix" id="titreChoixSTI2" style="position absolute; top:2%;">
-						Projet Scolaire
+						Projet Universitaire
 					</span>
 				</div>
 			</div>
 		</div>
 		<div id="projetsliste">
 			<div class="projets" id="mode">
-				<p class="pGauche">Un ami qui travail dans le domaine de la mode m'a demandé de faire la page d'accueil pour son site de mode.</br></br>
-					Ce site à était fait en HTML/CSS et aussi grâce à la librairie Jquery pour les quelques évènements comme le menu qui affichait les sous rubriques lors du passage de la sourie.
-					</br></br>Grâce à ce site, sa note à été de 18/20 :D
+				<p class="pGauche">Un ami qui travaille dans le domaine de la mode m'a demandé de faire la page d'accueil pour son site de mode.</br></br>
+					Ce site a était fait en HTML/CSS et aussi grâce à la librairie Jquery pour les quelques évènements comme le menu qui affichait les sous rubriques lors du passage de la sourie
 				</p>
 				<img src="./images/mode.jpg" alt="Photo site de mode" style="width:50%;height:80%; float:right;margin-top:3%; margin-right:5%; box-shadow:3px 3px 3px 3px #888888"/>
 			</div>
@@ -61,9 +60,9 @@
 			<div class="projets" id="rpg">
 				<p class="pDroit">Dans le cadre de mes études, j'ai dû réaliser un projet en groupe de 3 sur la création d'un jeux RPG nommé Ring en Java.</br></br>
 				Comme vous pouvez le voir sur les images, le jeux était basé principalement sur console. Grâce à notre travail d'équipe et notre motivation, 
-				nous avons eu le temps de faire quelques fenêtres graphiques qui rendait la manière de jouer largement plus agréable, pour rentrer les différentes statistiques du personnage par exemple.
+				nous avons eu le temps de faire quelques fenêtres graphiques qui rendaient la manière de jouer largement plus agréable, pour rentrer les différentes statistiques du personnage par exemple.
 				Le but était de créer ou importer un personnage et de le faire combattre contre un autre en réseau local. Avec un système d'expérience et d'évolution, le jeux était d'autant plus 
-				compétitif qu'amicale.
+				compétitif qu'amical.
 				</p>
 				<div id="slider">
 				</div>
@@ -78,15 +77,15 @@
 				<img src="./images/BlueNote1.png" alt="Photo site du restaurant Blue Note" style="width:50%;height:80%; float:right; margin-right:5%; margin-top:3%; box-shadow:3px 3px 3px 3px #888888" />
 			</div>
 			<div class="projets" id="othelo">
-				<p class="pDroit">J'ai dû réalisr un projet en groupe de 2 lors de mon premier semestre de DUT informatique.</br>
+				<p class="pDroit">J'ai dû réaliser un projet en groupe de 2 lors de mon premier semestre de DUT informatique.</br>
 				Ce projet était de créer un jeux nommé Réversi ou Othélo en langage C sur console.
 				</p>
 				<img src="./images/Othelo.png" alt="Photo du jeux Ohtelo sur console" style="width:30%;height:80%; float:left; margin-left:5%; margin-top:3%; box-shadow:3px 3px 3px 3px #888888" />
 			</div>
 			<div class ="projets" id="sti">
 				<p class ="pGauche"> Pour mon projet de BAC STI2D j'ai du réaliser un projet dont le sujet était l'ouverture d'un hayon de voiture sans contact et sans handicaper la personne tout en respectant l'environnement et la sécurité des alentours.</br></br>
-									Ma tâche à était principalement de m'occuper du capteur qui détecte un mouvement pour donner l'ordre d'ouvrir le hayon.</br>
-									La solution choisie à était de placer le capteur sous la voiture et de capter le passage du pied, un passage signifie l'ouverture du hayon, deux passages signifie la fermeture du hayon.</br>
+									Ma tâche a été principalement de m'occuper du capteur qui détecte un mouvement pour donner l'ordre d'ouvrir le hayon.</br>
+									La solution choisie a était de placer le capteur sous la voiture et de capter le passage du pied, un passage signifient l'ouverture du hayon, deux passages signifie la fermeture du hayon.</br>
 
 				</p>
 				<img src="./images/Capteur.png" alt="Photo capteur" style="width:50%;height:80%; float:right; margin-right:5%; margin-top:3%; box-shadow:3px 3px 3px 3px #888888" />
